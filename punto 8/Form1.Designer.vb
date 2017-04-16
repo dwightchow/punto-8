3cacac8111639d1b5d44a51611810fbe08256add
dwightchow <chowdwight@hotmail.com>
2017-04-15 22:57:01 -0500

    pun
    punto 8

---
+++ punto 8/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.contra = New System.Windows.Forms.TextBox()
+        Me.usuario = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'contra
+        '
+        Me.contra.Location = New System.Drawing.Point(188, 93)
+        Me.contra.Name = "contra"
+        Me.contra.Size = New System.Drawing.Size(100, 20)
+        Me.contra.TabIndex = 0
+        '
+        'usuario
+        '
+        Me.usuario.Location = New System.Drawing.Point(188, 39)
+        Me.usuario.Name = "usuario"
+        Me.usuario.Size = New System.Drawing.Size(100, 20)
+        Me.usuario.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(188, 157)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Ingresar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(60, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Usuario"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(63, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Contraseña"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(392, 316)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.usuario)
+        Me.Controls.Add(Me.contra)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents contra As System.Windows.Forms.TextBox
+    Friend WithEvents usuario As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ punto 8/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Dim ar As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If usuario.Text = "chow" And contra.Text = "hola1" Then
+            Form2.Show()
+
+        Else
+
+        End If
+        If ar = 3 Then
+            MsgBox("Usuario o contraseña incorrecta")
+            usuario.Text = ""
+            contra.Text = ""
+        End If
+        ar = ar + 1
+        If ar >= 3 Then End
+    End Sub
+End Class
